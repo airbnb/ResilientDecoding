59ef528bb679a89be29183b9a9c7afa7878e3f35
Dan Federman <dfed@me.com>
2020-04-09 10:04:41 -0700

    Add platforms to Package.swift

--- Package.swift
+++ Package.swift
@@ -3,6 +3,12 @@ import PackageDescription
 
 let package = Package(
   name: "ResilientDecoding",
+  platforms: [
+      .iOS(.v12),
+      .tvOS(.v12),
+      .watchOS(.v5),
+      .macOS(.v10_14),
+  ],
   products: [
     .library(
       name: "ResilientDecoding",
